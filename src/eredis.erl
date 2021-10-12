a9aa005a310ba0bba3c4f68b1a0168e772ce009f
Leo Wang <lwang@tigertext.com>
2021-10-12 15:46:08 +0800

    TY-127

--- src/eredis.erl
+++ src/eredis.erl
@@ -37,6 +37,10 @@ start_link() ->
 start_link(Host, Port) ->
     start_link(Host, Port, 0, "").
 
+start_link(Host, Port, Options) when Options =:= []; is_tuple(hd(Options)) ->
+    %% For ElastiCache
+    IsSSL = proplists:get_value(ssl, Options, false),
+    start_link(Host, Port, 0, "", 100, ?TIMEOUT, true, [], IsSSL);
 start_link(Host, Port, Database) ->
     start_link(Host, Port, Database, "").
 
